a07d6e35e379aaf2732c3dbb32d15467d5e7dd26
thomasmurphyittd <thomasmurphyittd@gmail.com>
2015-02-05 11:54:35 +0000

    //eeeeee

--- VPProjects/test/test.java
+++ VPProjects/test/test.java
@@ -2,7 +2,7 @@ package test;
 
 public class test {
 
-	private string name;
+	private String name;
 	private int IdNumber;
 	private int attribute;
 
@@ -10,36 +10,4 @@ public class test {
 		// TODO - implement test.test
 		throw new UnsupportedOperationException();
 	}
-
-	public string getName() {
-		return this.name;
-	}
-
-	/**
-	 * 
-	 * @param name
-	 */
-	public void setName(string name) {
-		this.name = name;
-	}
-
-	public int getIdNumber() {
-		// TODO - implement test.getIdNumber
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param IdNumber
-	 */
-	public void setIdNumber(int IdNumber) {
-		// TODO - implement test.setIdNumber
-		throw new UnsupportedOperationException();
-	}
-
-	public void operation() {
-		// TODO - implement test.operation
-		throw new UnsupportedOperationException();
-	}
-
 }
