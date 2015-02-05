b1aad566d97dcb73e0596f1aab09158ae04f24cc
thomasmurphyittd <thomasmurphyittd@gmail.com>
2015-02-05 11:17:37 +0000

    //Commit vpp folder

---
+++ VPProjects/test/test.java
@@ -0,0 +1,45 @@
+package test;
+
+public class test {
+
+	private string name;
+	private int IdNumber;
+	private int attribute;
+
+	public test() {
+		// TODO - implement test.test
+		throw new UnsupportedOperationException();
+	}
+
+	public string getName() {
+		return this.name;
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(string name) {
+		this.name = name;
+	}
+
+	public int getIdNumber() {
+		// TODO - implement test.getIdNumber
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param IdNumber
+	 */
+	public void setIdNumber(int IdNumber) {
+		// TODO - implement test.setIdNumber
+		throw new UnsupportedOperationException();
+	}
+
+	public void operation() {
+		// TODO - implement test.operation
+		throw new UnsupportedOperationException();
+	}
+
+}
